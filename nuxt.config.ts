93e87a7f515bb49e7027f2cd3c249e01531adba8
Pawan Paudel <pawanpaudel93@gmail.com>
2023-09-01 22:05:48 +0545

    fix: Update node polyfills include

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,10 @@ export default defineNuxtConfig({
     },
   },
   vite: {
-    plugins: [nodePolyfills()],
+    plugins: [
+      nodePolyfills({
+        include: ["buffer", "crypto", "stream", "util"],
+      }),
+    ],
   },
 });
